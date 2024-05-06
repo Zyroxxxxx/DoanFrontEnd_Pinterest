e5d46ea2bb370e5cad7831ab00109a282c471b7e
Zyroxxxxx <127403236+Zyroxxxxx@users.noreply.github.com>
2024-05-06 11:05:40 +0700

    Add files via upload

---
+++ DoanFrontEnd-Pinterest/app.js
@@ -0,0 +1,107 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'Galaxy S22 Ultra 5G',
+        image: '1.PNG',
+        price: 30990000
+    },
+    {
+        id: 2,
+        name: 'Điện thoại Xiaomi Redmi 12 4GB',
+        image: '2.PNG',
+        price: 4290000
+    },
+    {
+        id: 3,
+        name: 'Samsung Galaxy Z Flip4 5G 128GB',
+        image: '3.PNG',
+        price: 15990000
+    },
+    {
+        id: 4,
+        name: 'Điện thoại realme C55 6GB',
+        image: '4.PNG',
+        price: 4790000
+    },
+    {
+        id: 5,
+        name: 'Laptop Asus TUF Gaming F15 FX506HF',
+        image: '5.PNG',
+        price: 19690000
+    },
+    {
+        id: 6,
+        name: 'Xiaomi Redmi Watch 3',
+        image: '6.PNG',
+        price: 2590000
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Chọn để thêm</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
